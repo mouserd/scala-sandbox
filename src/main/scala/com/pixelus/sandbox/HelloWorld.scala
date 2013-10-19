3a8dca08196159d36ca993d6e76d50944d6c27d6
David Mouser <david.mouser@gmail.com>
2013-10-19 22:58:32 +1100

    Refactored to a class instead of an object.

--- src/main/scala/com/pixelus/sandbox/HelloWorld.scala
+++ src/main/scala/com/pixelus/sandbox/HelloWorld.scala
@@ -1,16 +1,8 @@
 package com.pixelus.sandbox
 
-object HelloWorld {
+class HelloWorld {
 
   def sayHello(name:String): String = {
     s"Hello ${name}!";
   }
-
-  def main(args:Array[String]) = {
-    var name = if (args.length > 0) args(1) else "Dave";
-
-    println(sayHello(name))
-  }
 }
-
-//HelloWorld.sayHello("Dave")
